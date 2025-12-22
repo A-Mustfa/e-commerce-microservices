762af7d5f73ffcda60c05c1836d21e12be04adce
amustfa <amustafa@Kn-it.com>
2025-12-22 16:20:31 +0200

    updated notification service

--- services/notification-service/src/main/java/org/ecommerce/notificationservice/kafka/order/NotificationConsumer.java
+++ services/notification-service/src/main/java/org/ecommerce/notificationservice/kafka/order/NotificationConsumer.java
@@ -3,7 +3,6 @@ package org.ecommerce.notificationservice.kafka.order;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ecommerce.notificationservice.email.EmailService;
 import org.ecommerce.notificationservice.notification.Notification;
 import org.ecommerce.notificationservice.notification.NotificationRepository;
 import org.ecommerce.notificationservice.notification.NotificationType;
@@ -18,26 +17,19 @@ import java.time.LocalDateTime;
 public class NotificationConsumer {
 
     private final NotificationRepository notificationRepository;
-    private final EmailService emailService;
 
     @KafkaListener(topics = "order-topic")
     public void consumeOrderConfirmation(OrderConfirmation orderConfirmation) throws MessagingException {
         log.info("Received Order Confirmation: {}", orderConfirmation);
-        notificationRepository.save(
-                Notification.builder()
-                        .type(NotificationType.ORDER_CONFIRMATION)
-                        .date(LocalDateTime.now())
-                        .orderId(orderConfirmation.orderId())
-                        .customerEmail(orderConfirmation.customerEmail())
-                        .totalAmount(orderConfirmation.totalAmount())
-                        .build()
-        );
-        var customerName = orderConfirmation.customer().getName();
-        emailService.sendOrderEmail(orderConfirmation.customerEmail(),
-                customerName,
-                orderConfirmation.totalAmount(),
-                orderConfirmation.orderId()
-        );
+        Notification notification = Notification.builder()
+                .type(NotificationType.ORDER_CONFIRMATION)
+                .date(LocalDateTime.now())
+                .orderId(orderConfirmation.orderId())
+                .customerEmail(orderConfirmation.customerEmail())
+                .totalAmount(orderConfirmation.totalAmount())
+                .build();
+        notificationRepository.save(notification);
+        log.info("notification sent successfully {}", notification);
     }
 
 }
