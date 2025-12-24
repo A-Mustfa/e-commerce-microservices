8773c771e839f64e6dccc0cafbc9b4bf40c273d0
amustfa <amustafa@Kn-it.com>
2025-12-24 16:37:35 +0200

    added utils to scanPackages

--- services/auth-server/src/main/java/org/ecommerce/authserver/AuthServerApplication.java
+++ services/auth-server/src/main/java/org/ecommerce/authserver/AuthServerApplication.java
@@ -14,7 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.ecommerce.authserver","commons.utils"})
 @EnableConfigurationProperties(RsaKeys.class)
 @EnableFeignClients
 @OpenAPIDefinition(
